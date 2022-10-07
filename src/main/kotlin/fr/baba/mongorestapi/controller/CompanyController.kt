d0380e46e9ea21765b9ac98ec536b76e17dcf35b
BHardelin <baptiste.hardelin@adeo.com>
2022-10-07 15:04:14 +0200

    Can dispaly only one company by their id

--- src/main/kotlin/fr/baba/mongorestapi/controller/CompanyController.kt
+++ src/main/kotlin/fr/baba/mongorestapi/controller/CompanyController.kt
@@ -5,6 +5,7 @@ import fr.baba.mongorestapi.service.request.CompanyRequest
 import fr.baba.mongorestapi.service.servcie.CompanyService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -28,6 +29,16 @@ class CompantController(
         val companies = companyService.findAll()
 
         return ResponseEntity.ok(companies.map { CompanyResponse.fromEntity(it) })
+    }
+
+    @GetMapping("/{id}")
+    fun findCompanyById(@PathVariable id: String): ResponseEntity<CompanyResponse> {
+        val company = companyService.findById(id)
+
+        return ResponseEntity
+            .ok(
+                CompanyResponse.fromEntity(company)
+            )
     }
 
 
